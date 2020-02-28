81265f76883585c57d0afb24f34f9cec7e329fd0
dillonp23 <dillonp.lambda@gmail.com>
2020-02-27 20:31:03 -0800

    Add travelerController class. Ass temporary "newUser" struct for testing purposes. Add register new user function to TravelerController.

--- KidsFly/KidsFly/Model Controllers/TravelerController.swift
+++ KidsFly/KidsFly/Model Controllers/TravelerController.swift
@@ -8,11 +8,46 @@
 
 import Foundation
 
+struct NewUser: Codable {
+    let email: String
+    let password: String
+}
+
 class TravelerController {
     
-    private let baseURL = URL(string: "https://kidsfly-lambda2.herokuapp.com/")
+    private let baseURL = URL(string: "https://kidsfly-lambda2.herokuapp.com/")!
     
     
+    func registerNewUser(for email: String, with password: String, completion: @escaping (Error?) -> Void) {
+        
+        let newUser = NewUser(email: email, password: password)
+        
+        let registerNewUserURL = baseURL.appendingPathComponent("localhost:5000/api/auth/register")
+        
+        var request = URLRequest(url: registerNewUserURL)
+        request.httpMethod = HTTPMethod.post
+        
+        let encoder = JSONEncoder()
+        
+        do {
+            let data = try encoder.encode(newUser)
+            request.httpBody = data
+        } catch {
+            print("Error encoding data: \(error)")
+            return
+        }
+        
+        URLSession.shared.dataTask(with: request) { _, _, error in
+            if let error = error {
+                print("Error registering new user: \(error)")
+                completion(error)
+                return
+            }
+            
+            completion(nil)
+            
+        }.resume()
+    }
     
     
 }
