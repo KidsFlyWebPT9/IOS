6b569153f726ded7c852c3c3b30c62f828e299ac
dillonp23 <dillonp.lambda@gmail.com>
2020-02-27 20:02:19 -0800

    Add extension on URLSession that conforms to NetworkDataLoader protocol. Add MockLoader struct that conforms to NetworkDataLoader protocol.

--- KidsFly/KidsFly/Helper/NetworkDataLoader.swift
+++ KidsFly/KidsFly/Helper/NetworkDataLoader.swift
@@ -10,4 +10,49 @@ import Foundation
 
 protocol NetworkDataLoader {
     func loadData(with request: URLRequest, completion: @escaping (Data?, Error?) -> Void)
+    
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void)
+}
+
+
+extension URLSession: NetworkDataLoader {
+    
+    func loadData(with request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        
+        let dataTask = URLSession.shared.dataTask(with: request) { (data, _, error) in
+            completion(data, error)
+        }
+        
+        dataTask.resume()
+    }
+    
+    
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        let dataTask = URLSession.shared.dataTask(with: url) { (data, _, error) in
+            completion(data, error)
+        }
+        
+        dataTask.resume()
+    }
+    
+}
+
+
+struct MockLoader: NetworkDataLoader {
+    
+    var data: Data?
+    var error: Error?
+    
+    func loadData(from url: URL, completion: @escaping (Data?, Error?) -> Void) {
+        DispatchQueue.global().async {
+            completion(self.data, self.error)
+        }
+    }
+    
+    func loadData(with request: URLRequest, completion: @escaping (Data?, Error?) -> Void) {
+        DispatchQueue.global().async {
+            completion(self.data, self.error)
+        }
+    }
+    
 }
---
+++ KidsFly/KidsFly/Model Controllers/TravelerController.swift
@@ -0,0 +1,18 @@
+//
+//  TravelerController.swift
+//  KidsFly
+//
+//  Created by Dillon P on 2/27/20.
+//  Copyright © 2020 Enayatullah Naseri. All rights reserved.
+//
+
+import Foundation
+
+class TravelerController {
+    
+    private let baseURL = URL(string: "https://kidsfly-lambda2.herokuapp.com/")
+    
+    
+    
+    
+}
