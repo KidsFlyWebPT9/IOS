56e4f4fc02d63fa5d3ad9ef4cea2e35afb6b53df
John Holowesko <jholowesko@gmail.com>
2020-03-03 19:16:23 -0500

    Pre-Dinner push (With errors)

--- KidsFly/KidsFly/CoreData/Connection+Convenience.swift
+++ KidsFly/KidsFly/CoreData/Connection+Convenience.swift
@@ -26,4 +26,32 @@ extension Connection {
         self.firstName = firstName
         self.address = address
     }
+    
+    /// Creates Connection from ConnectionRepresentation Data (Firebase JSON data)
+    @discardableResult
+    convenience init?(connectionRepresentation: ConnectionRepresentation, context: NSManagedObjectContext = CoreDataStack.shared.mainContext) {
+        
+        guard let identifierString = connectionRepresentation.identifier,
+            let identifier = UUID(uuidString: identifierString) else { return nil }
+        
+        self.init(context: context,
+                  username = connectionRepresentation.username,
+                  password = connectionRepresentation.password,
+                  lastName = connectionRepresentation.lastName,
+                  identifier = connectionRepresentation.identifier,
+                  homeAirport = connectionRepresentation.homeAirport,
+                  firstName = connectionRepresenation.firstName,
+                  address = connectionRepresenation.address)
+    }
+    
+
+    // MARK: - Objects
+
+    /// Object used to pass Connections back and forward to Firebase in .JSON format
+    var connectionRepresenation: ConnectionRepresenation? {
+        guard let identifier = identifier else { return nil }
+
+        #warning("not Complete")
+        return ConnectionRepresentation(
+    }
 }
