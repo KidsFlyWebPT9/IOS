ebea834a37498b444ab1e30e4d72cfd25af6a071
dillonp23 <dillonp.lambda@gmail.com>
2020-03-03 20:08:24 -0800

    Add tripController and initial boilerplate for CRUD methods

---
+++ KidsFly/KidsFly/Model Controllers/TripController.swift
@@ -0,0 +1,47 @@
+//
+//  TripController.swift
+//  KidsFly
+//
+//  Created by Dillon P on 3/3/20.
+//  Copyright © 2020 Enayatullah Naseri. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+struct TripRepresentation: Codable {
+    
+}
+
+class TripController {
+    
+    
+    func createNewTrip(withTrip: TripRepresentation, completion: @escaping (Error?) -> Void) {
+        
+    }
+    
+    func getAllTrips(forUser: User, completion: @escaping (Error?) -> Void) {
+        
+    }
+    
+    
+    func getSingleTrip(trip: TripRepresentation, forUser: User, completion: @escaping (Error?) -> Void) {
+        
+    }
+    
+    
+    func updateTrip(withTrip: TripRepresentation, forUser: User, completion: @escaping (Error?) -> Void) {
+        
+    }
+    
+    
+    func deleteTrip(trip: TripRepresentation, forUser: User, completion: @escaping (Error?) -> Void) {
+        
+    }
+    
+    
+    
+    
+    
+    
+}
